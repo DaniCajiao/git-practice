865b5281f364273eea0cb97253d07b51fb8738ba
Daniel Cajiao <cajiao.dani@gmail.com>
2020-02-19 14:10:05 -0500

    fake teamwork 2

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hello world! This is change 2" << endl;
-    cout << "This is fake teamwork 1" << endl;
+    cout << "This is fake teamwork 2" << endl;
     return 0;
 }
